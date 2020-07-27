c9ba03498be963ca229b7ae5ff589f91436cc675
Vladim <vladim.volovenko@gmail.com>
2020-07-27 19:28:49 +0300

    49 lesson mapping users and show it

---
+++ src/components/users/usersContainer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Users from './Users';
+import {followAC, unfollowAC, setUsersAC} from './../../redux/users_reduser';
+
+
+let mapStateToProps = (state) => {
+    return {
+        users: state.usersReducer.users
+    }
+}
+
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        follow: (userId) => {
+            dispatch(followAC(userId));
+        },
+        unfollow: (userId) => {
+            dispatch(unfollowAC(userId));
+        },
+        setUsers: (users) => {
+            dispatch(setUsersAC(users));
+        }
+    }
+}
+
+
+
+let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+
+export default UsersContainer;
