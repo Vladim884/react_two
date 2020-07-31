30413c083e58a11d819087f62db21e9c50838dac
unknown <vladim.volovenko.com>
2020-07-31 20:27:04 +0300

    new change in windows-10

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Users = (props) => {
     debugger;
   return  <div>
+    <h1>user-user</h1>
       {
       props.users.map(u => <div key={u.id}>{u.fullName}</div>)
     }
