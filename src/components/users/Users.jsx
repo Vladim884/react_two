8dd81fb3fb8b3d49a3169c0e38b662548c6f51c1
Vladimir Volovenko <vladim.volovenko.com>
2020-08-07 23:45:07 +0300

    lesson 49 usersPage(begining friends-conception)

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React from "react";
+import s from "./users.module.css";
+import * as axios from "axios";
 
 const Users = (props) => {
-    debugger;
-  return  <div>
-    <h1>user-user connected vs code</h1>
-      {
-      props.users.map(u => <div key={u.id}>{u.fullName}</div>)
+  let apiUrl = 'https://social-network.samuraijs.com/api/1.0/';
+  axios.get(`${apiUrl}users`).then((response => {
+    if (props.users.length === 0){
+      props.setUsers(response.data.items);
+      debugger;
     }
-  </div>
-  
-}
+  }));
 
-export default Users
 
-      
   
+  
+  return (
+    <div className={s.users_block}>
+      <h1>Users</h1>
+      {props.users.map((u) => (
+        
+        <div key={u.id} className={s.user}>
+          <div className={s.ava}>
+            <img src={u.photos.small}
+              // width="40%"
+              alt="user"
+            />
+            <div>{u.name}</div>
+          </div>
+          {u.followed ?  (
+            <button onClick={() => {props.unfollow(u.id)}}>follow</button>
+          ) :  ( 
+            <button onClick={() => {props.follow(u.id)}}>unfollow</button>
+          )}
+          <div>{u.status}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
