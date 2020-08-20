92408ebc64e0624f3f15fa654bfaa73a4838b4a8
Vladimir Volovenko <vladim.volovenko.com>
2020-08-21 00:58:50 +0300

    DAL, axios.create userAPi.getUsers()

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,25 @@
 import * as axios from "axios";
 
+const instance = axios.create({
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'e4fafe2c-01ea-415d-962d-f70071c3ef3a'
+    }
+});
+
+const baseUrl = 'https://social-network.samuraijs.com/api/1.0/';
+
+
+export const userAPi = {
+    getUsers(currentPage=38, pageSize=100) {
+        return instance.get(
+          baseUrl + `users?page=${currentPage}&count=${pageSize}`,
+        ).then(response => response.data);
+      },
+}
+
 export const getUsers = (currentPage=38, pageSize=100) => {
-  return axios.get(
-    `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-    { withCredentials: true }
-  );
+  return instance.get(
+    baseUrl + `users?page=${currentPage}&count=${pageSize}`,
+  ).then(response => response.data);
 };
